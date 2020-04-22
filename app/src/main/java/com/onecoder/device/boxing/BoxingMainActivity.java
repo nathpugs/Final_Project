556ca38fd7e6771722ab6a0a6b4b3a66e7813524
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2020-04-22 17:16:29 +0100

    Timer keeps counting when app is closed

--- app/src/main/java/com/onecoder/device/boxing/BoxingMainActivity.java
+++ app/src/main/java/com/onecoder/device/boxing/BoxingMainActivity.java
@@ -1,6 +1,7 @@
 package com.onecoder.device.boxing;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Bundle;
@@ -82,12 +83,17 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
 
     // Timer Code - Variables
     private static final long START_TIME_IN_MILLIS = 60000;
+
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
     private Button mButtonReset;
+
     private CountDownTimer mCountDownTimer;
+
     private boolean mTimerRunning;
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    private long mTimeLeftInMillis;
+    private long mEndTime;
 
 
 
@@ -149,8 +155,10 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
         // Timer Code [ START ]
 
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
         mButtonStartPause = findViewById(R.id.button_start_pause);
         mButtonReset = findViewById(R.id.button_reset);
+
 
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -169,7 +177,6 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
                 resetTimer();
             }
         });
-        updateCountDownText();
 
         // Timer Code - [ END ]
     }
@@ -177,6 +184,8 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
     // Timer Code - Methods [ START ]
 
     private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -187,29 +196,24 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
             @Override
             public void onFinish() {
                 mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
+                updateButtons();
             }
         }.start();
 
         mTimerRunning = true;
-        mButtonStartPause.setText("pause");
-        mButtonReset.setVisibility(View.INVISIBLE);
+        updateButtons();
     }
 
     private void pauseTimer() {
         mCountDownTimer.cancel();
         mTimerRunning = false;
-        mButtonStartPause.setText("Start");
-        mButtonReset.setVisibility(View.VISIBLE);
+        updateButtons();
     }
 
     private void resetTimer() {
         mTimeLeftInMillis = START_TIME_IN_MILLIS;
         updateCountDownText();
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
+        updateButtons();
     }
 
     private void updateCountDownText() {
@@ -220,6 +224,74 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
 
         mTextViewCountDown.setText(timeLeftFormatted);
     }
+
+    private void updateButtons() {
+        if (mTimerRunning) {
+            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        } else {
+            mButtonStartPause.setText("Start");
+
+            if (mTimeLeftInMillis < 1000) {
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+            } else {
+                mButtonStartPause.setVisibility(View.VISIBLE);
+            }
+
+            if (mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+                mButtonReset.setVisibility(View.VISIBLE);
+            } else {
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("millisLeft", mTimeLeftInMillis);
+        editor.putBoolean("timerRunning", mTimerRunning);
+        editor.putLong("endTime", mEndTime);
+
+        editor.apply();
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+        mTimeLeftInMillis = prefs.getLong("millisLeft", START_TIME_IN_MILLIS);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCountDownText();
+        updateButtons();
+
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+                updateButtons();
+            } else {
+                startTimer();
+            }
+        }
+    }
+
+
 
     // Timer Code - Methods [ END ]
 
@@ -566,11 +638,6 @@ public class BoxingMainActivity extends BaseActivity implements View.OnClickList
         return ret.toString();
     }
 
-    @Override
-    protected void onStop() {
-        showDialog(null, false);
-        super.onStop();
-    }
 
     @Override
     public void run() {
