4993a6e08122297ab48f786a2e621bd87bb7d45b
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2020-04-30 22:38:09 +0100

    Connect sensor screen with custom menu

--- app/src/main/java/com/onecoder/device/devicemanager/DeviceManagerActivity.java
+++ app/src/main/java/com/onecoder/device/devicemanager/DeviceManagerActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.onecoder.device.ManagerContainer;
@@ -77,6 +79,14 @@ public class DeviceManagerActivity extends BaseActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_manager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
@@ -122,14 +132,16 @@ public class DeviceManagerActivity extends BaseActivity implements View.OnClickL
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 0, 0, "+");
-        return super.onCreateOptionsMenu(menu);
+        //menu.add(0, 0, 0, "+");
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.connect_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case 0:
+            case R.id.bluetoothIcon:
                 startScanDevPage();
                 break;
 
