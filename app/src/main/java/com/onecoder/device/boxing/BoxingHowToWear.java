85614f898eab68182e04fa995f7fe5e53d8d211a
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2020-05-01 12:41:31 +0100

    How to wear section now includes images

--- app/src/main/java/com/onecoder/device/boxing/BoxingHowToWear.java
+++ app/src/main/java/com/onecoder/device/boxing/BoxingHowToWear.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 import com.onecoder.device.R;
@@ -20,6 +21,18 @@ public class BoxingHowToWear extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.boxing_how_to_wear);
+
+
+        ImageView firstImage = (ImageView) findViewById(R.id.firstImage);
+        ImageView secondImage = (ImageView) findViewById(R.id.secondImage);
+        ImageView thirdImage = (ImageView) findViewById(R.id.thirdImage);
+        ImageView fourthImage = (ImageView) findViewById(R.id.fourthImage);
+
+
+        firstImage.setImageResource(R.drawable.tutorial_1);
+        secondImage.setImageResource(R.drawable.tutorial_2);
+        thirdImage.setImageResource(R.drawable.tutorial_3);
+        fourthImage.setImageResource(R.drawable.tutorial_4);
 
         button = (Button) findViewById(R.id.howToBtn);
         button.setOnClickListener(new View.OnClickListener() {
